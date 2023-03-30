5bb46c9bf748f80d582c25af1c4bcfffdf92edcd
Tobias Spronk <tobias.spronk@siemens-healthineers.com>
2023-03-30 15:26:24 +0200

    after vid 2

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// for use by Hazel applications
+
+#include "Hazel/Application.h"
+#include <cstdio>
+
+// ---------------- Entry Point---------------------//
+#include "Hazel/EntryPoint.h"
+// ------------------------------------------------//
