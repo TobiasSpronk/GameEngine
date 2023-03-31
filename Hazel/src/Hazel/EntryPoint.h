92dda7eeee37f26f4487a3f051c2b41a41bb6a6a
Tobias Spronk <tobias.spronk@siemens-healthineers.com>
2023-03-31 09:36:12 +0200

    included logging functionality

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,6 +7,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var = {0}", a);
+
+
 	printf("Welcome to hazel engine");
 	auto app = Hazel::CreateApplication();
 	app->run();
